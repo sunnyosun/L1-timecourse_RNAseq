e0f850748d04ce72cbf4e43c54279adbbb74ab98
sunnysun <xs338@nyu.edu>
2018-04-11 17:50:34 -0400

    Running the functions in EdgeR-norm.R

---
+++ EdgeR_norm_run.R
@@ -0,0 +1,10 @@
+# Running EdgeR normalization from functions in EdgeR_norm.R
+source("/Users/sunnysun/Documents/GitHub/L1-timecourse_RNAseq/EdgeR_norm.R")
+
+
+
+# merge counts files into one file
+wkdir='/Volumes/My Book for Mac/rnaseq_l1_timecourse/star_out'
+setwd(wkdir)
+name=read.table('../rnaseq/name.txt',sep='\t',stringsAsFactors = F)
+outname='count_merged.txt'
